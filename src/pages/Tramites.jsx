aa3668f6da7a29f97a172fa754bcf4f0fe8a96af
JaviMx <javimxmine@hotmail.com>
2022-06-28 20:56:43 -0500

    Ventana tramites

--- src/pages/Tramites.jsx
+++ src/pages/Tramites.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const Tramites = () => {
   return (
-    <div>Tramites</div>
+    <div className="text-center">
+      <h2 className="text-2xl font-bold -mt-10 mb-20 relative">Tramites</h2>
+      <Link to={''} className="border-solid border-2 border-orange-500 block p-2 bg-orange-400 font-bold mb-4">
+        Tomate la foto
+      </Link>
+      <Link to={''} className="border-solid border-2 border-orange-500 block p-2 bg-orange-400 font-bold mb-4">
+        Actualizar tu sello
+      </Link>
+      <Link to={''} className="border-solid border-2 border-orange-500 block p-2 bg-orange-400 font-bold mb-4">
+        Revalida tu pase escolar
+      </Link>
+      <Link to={''} className="border-solid border-2 border-orange-500 block p-2 bg-orange-400 font-bold mb-4">
+        Paga tu pase escolar
+      </Link>
+
+
+    </div>
   )
 }
 
